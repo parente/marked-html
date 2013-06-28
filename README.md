c3b6078a9d579b9a2b2927612498c57fe29ec26b
Peter Parente <parente@cs.unc.edu>
2013-06-27 21:27:11 -0400

    Metadata, tests

---
+++ README.md
@@ -0,0 +1,11 @@
+# marked-html
+
+Renders a Markdown file as a complete HTML document based on a mustache template. Uses [marked](https://github.com/chjj/marked) to transform and [highlight.js](http://softwaremaniacs.org/soft/highlight/en/) to syntax highlight code.
+
+Inspired by [markdown-html](https://github.com/fragphace/markdown-html).
+
+## License
+
+Copyright (c) 2013, Peter Parente under the MIT License.
+
+See the LICENSE file for details.
---
+++ test/test.js
@@ -0,0 +1,112 @@
+var should = require('should'),
+    fs = require('fs'),
+    path = require('path'),
+    mh = require('../lib/marked-html'),
+    spawn = require('child_process').spawn;
+
+var md_fn = __dirname+'/test.md';
+var md = fs.readFileSync(md_fn, 'utf-8');
+
+describe('render', function() {
+    var template = fs.readFileSync(__dirname+'/../template/default.mustache', 'utf-8');
+
+    it('should output html', function() {
+        var rv = mh.render(md, template, {});
+        rv.should.be.a('string');
+        rv.should.match(/doctype html/ig);
+        rv.should.match(/<title><\/title>/ig);
+    });
+    it('should have a title', function() {
+        var args = {title: 'my title'};
+        var rv = mh.render(md, template, args);
+        rv.should.match(/<title>my title<\/title>/ig);
+    });
+    it('should have linked css', function() {
+        var args = {css_link: 'foobar.com/css'};
+        var rv = mh.render(md, template, args);
+        rv.should.match(/<link rel="stylesheet" type="text\/css" href="foobar.com\/css" \/>/ig);
+        rv.should.not.match(/<style/ig);
+    });
+    it('should have embedded css', function() {
+        var args = {css_inline: __dirname+'/../template/default.css'};
+        var rv = mh.render(md, template, args);
+        rv.should.not.match(/<link/ig);
+        rv.should.match(/<style/ig);
+    });
+    it('should have a hljs theme', function() {
+        var args = {css_hljs_theme: 'foobar.com/css'};
+        var rv = mh.render(md, template, args);
+        rv.should.match(/<link rel="stylesheet" type="text\/css" href="foobar.com\/css" \/>/ig);
+    });
+});
+
+describe('run', function() {
+    var bin = __dirname+'/../bin/marked-html',
+        expected_length = 6141;
+    it('should read a md file, write html to stdout', function(done) {
+        var output = '';
+        var c = spawn(bin, ['-i', md_fn]);
+        c.stdout.setEncoding('utf8');
+        c.stdout.on('data', function(data) {
+            output += data;
+        });
+        c.on('close', function(code) {
+            code.should.equal(0);
+            output.should.have.length(expected_length);
+            done();
+        });
+    });
+    it('should read a md file, write a html file', function(done) {
+        var output = '/tmp/test.html';
+        var c = spawn(bin, ['-i', md_fn, '-o', output]);
+        c.on('close', function(code) {
+            code.should.equal(0);
+            output = fs.readFileSync(output, 'utf-8');
+            output.should.have.length(expected_length);
+            done();
+        });
+    });
+    it('should read md from stdin, write html to stdout', function(done) {
+        var output = '';
+        var c = spawn(bin);
+        c.stdout.setEncoding('utf8');
+        c.stdout.on('data', function(data) {
+            output += data;
+        });
+        c.on('close', function(code) {
+            code.should.equal(0);
+            // no default title of test.md filename when stdin
+            output.should.have.length(expected_length-7);
+            done();
+        });
+        c.stdin.write(md);
+        c.stdin.end();
+    });
+    it('should read md from stdin, write a html file', function(done) {
+        var output = '/tmp/test.html';
+        var c = spawn(bin, ['-o', output]);
+        c.on('close', function(code) {
+            code.should.equal(0);
+            output = fs.readFileSync(output, 'utf-8');
+            // no default title of test.md filename when stdin
+            output.should.have.length(expected_length-7);
+            done();
+        });
+        c.stdin.write(md);
+        c.stdin.end();
+    });
+    it('should show help', function(done) {
+        var output = '';
+        var c = spawn(bin, ['-h']);
+        c.stderr.on('data', function(data) {
+            output += data;
+        });
+        c.on('close', function(code) {
+            code.should.equal(0);
+            output.should.match(/usage/gi);
+            output.should.match(/--output, -o/gi);
+            output.should.match(/--help, -h/gi);
+            done();
+        });
+    });
+});
